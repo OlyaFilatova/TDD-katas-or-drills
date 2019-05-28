13e4e894250b7d8e73a21c76bb792a6f4a8c382d
Olga Filatova <olyafilatov@gmail.com>
2019-05-28 10:16:19 +0300

    Red Phase. First test. Added roll loop.

--- spec/test-bowling-game.spec.js
+++ spec/test-bowling-game.spec.js
@@ -3,5 +3,8 @@ let Game = require("../app/bowling-game.js").Game;
 describe("Test bowling game", function(){
 	it("Test gutter game", function(){
 		let game = new Game();
+		for(let i = 0; i < 20; i++) {
+			game.roll(0);
+		}
 	});
 });
