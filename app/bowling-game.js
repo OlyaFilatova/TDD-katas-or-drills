af36d6a7be7adacfb334765ac1d3addc3b3a0cdb
Olga Filatova <olyafilatov@gmail.com>
2019-05-28 11:22:57 +0300

    Green phase. Third test. Wrote adding of the spare bonus

--- app/bowling-game.js
+++ app/bowling-game.js
@@ -8,7 +8,11 @@ let Game = function(){
 		let score = 0;
 		let frameIndex = 0;
 		for(let frame = 0; frame < 10; frame++) {
-			score += rolls[frameIndex] + rolls[frameIndex+1];
+			if(rolls[frameIndex] + rolls[frameIndex+1] == 10) {
+				score += 10 + rolls[frameIndex+2];
+			}else{
+				score += rolls[frameIndex] + rolls[frameIndex+1];
+			}
 			frameIndex += 2;
 		}
 		return score;
