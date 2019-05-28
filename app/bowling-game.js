7bb818159f12e1438df72563e86ddace07a9c0e1
Olga Filatova <olyafilatov@gmail.com>
2019-05-28 10:15:26 +0300

    Green Phase. First test. Created Game class

---
+++ app/bowling-game.js
@@ -0,0 +1,3 @@
+let Game = function(){};
+
+module.exports.Game = Game;
--- spec/test-bowling-game.spec.js
+++ spec/test-bowling-game.spec.js
@@ -1,3 +1,5 @@
+let Game = require("../app/bowling-game.js").Game;
+
 describe("Test bowling game", function(){
 	it("Test gutter game", function(){
 		let game = new Game();
